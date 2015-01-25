33f20b66cc5e853a28e6e227d47d5c849502bcb2
timaxapa <timaxapa@gmail.com>
2015-01-26 02:17:10 +0300

    Preparing

---
+++ application/controllers/main.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+
+	public function index()
+	{
+		$data['view'] = "main/main";
+		$data['page_title'] = "Главная";
+
+		$this->load->view('view', $data);
+	}
+}
---
+++ application/views/view.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        <?php echo isset($page_title) ? $page_title : "";?>
+    </title>
+
+    <!-- Bootstrap -->
+    <link href="<?=base_url()?>public/css/bootstrap.min.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+    <script src="<?=base_url()?>public/js/html5shiv.min.js"></script>
+    <script src="<?=base_url()?>public/js/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+    <?php
+        if (isset($view)) {
+            $this->load->view($view);
+        }
+    ?>
+
+    <script src="<?=base_url()?>public/js/jquery.min.js"></script>
+    <script src="<?=base_url()?>public/js/bootstrap.min.js"></script>
+</body>
+</html>
